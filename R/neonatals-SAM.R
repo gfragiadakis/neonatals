abb9ef7dddcbc8db3396c4edaf63e30fcf91f959
gfragiadakis <gabi.fragiadakis@gmail.com>
2016-10-04 14:28:40 -0700

    Function cleanup

--- R/neonatals-SAM.R
+++ R/neonatals-SAM.R
@@ -4,6 +4,7 @@
 
 library(dplyr)
 library(samr)
+library(reshape2)
 
 SAMDirectory <- "~/Documents/neonatal/SAM/"
 # generate response varaible y for Two Class Paired (Mom and Baby)
@@ -25,27 +26,34 @@ Unstim_df <- unstim_df
 
 dfs <- list("LPS" = LPS_df, "Cocktail" = Cocktail_df, "Unstim" = Unstim_df)
 
-output <- list()
-for (i in 1:length(dfs)){
-  df <- dfs[[i]]
-  x <- df[,!(names(df) %in% annotation_columns)]
-  model <-  SAM(t(x), y, genenames = colnames(x), resp.type = "Two class paired", nperms = 1000, fdr.output = 0.01)
-  sig.up <- model$siggenes.table$genes.up[model$siggenes.table$genes.up[,"q-value(%)"] == "0", ]
-  sig.lo <- model$siggenes.table$genes.lo[model$siggenes.table$genes.lo[,"q-value(%)"] == "0", ]
-  sigs <- rbind(sig.up, sig.lo)
-  write.csv(sigs, file = paste(SAMDirectory, names(dfs)[i], "_fdr_0.01.csv", sep = ""))
-  output[[names(dfs)[i]]] <- sigs
-  
-  sig_df <- data.frame(sigs)
-  feat_vals <- cbind(Individual = df$Individual, df[, colnames(df) %in% sig_df$Gene.ID])
-  write.csv(feat_vals, file = paste(SAMDirectory, names(dfs)[i], "_feature_values_fdr_0.01.csv", sep = ""))
-  
-  df <- melt(df)
-  
-  df <- df[df[, "variable"] %in% sig_df$Gene.ID,]
-  ggplot(df, aes(variable, value)) + geom_boxplot(aes(colour = SampleType)) + theme(axis.text = element_text(size = rel(1), angle = 90)) + ggtitle(names(dfs)[i])
-  ggsave(filename = paste(SAMDirectory, names(dfs)[i], "_sig_plot.pdf", sep = ""), width = 30, height = 20)
+neonatal_SAM <- function(dfs, y, resp.type, SAMDirectory){
+  output <- list()
+  for (i in 1:length(dfs)){
+    df <- dfs[[i]]
+    x <- df[,!(names(df) %in% annotation_columns)]
+    model <-  SAM(t(x), y, genenames = colnames(x), resp.type = resp.type, nperms = 1000, fdr.output = 0.01)
+    sig.up <- model$siggenes.table$genes.up[model$siggenes.table$genes.up[,"q-value(%)"] == "0", ]
+    sig.lo <- model$siggenes.table$genes.lo[model$siggenes.table$genes.lo[,"q-value(%)"] == "0", ]
+    sigs <- rbind(sig.up, sig.lo)
+    write.csv(sigs, file = paste(SAMDirectory, names(dfs)[i], "_fdr_0.01.csv", sep = ""))
+    output[[names(dfs)[i]]] <- sigs
+    
+    sig_df <- data.frame(sigs)
+    feat_vals <- cbind(Individual = df$Individual, df[, colnames(df) %in% sig_df$Gene.ID])
+    write.csv(feat_vals, file = paste(SAMDirectory, names(dfs)[i], "_feature_values_fdr_0.01.csv", sep = ""))
+    
+    df <- melt(df)
+    
+    df <- df[df[, "variable"] %in% sig_df$Gene.ID,]
+    ggplot(df, aes(variable, value)) + geom_boxplot(aes(colour = SampleType)) + theme(axis.text = element_text(size = rel(1), angle = 90)) + ggtitle(names(dfs)[i])
+    ggsave(filename = paste(SAMDirectory, names(dfs)[i], "_sig_plot.pdf", sep = ""), width = 30, height = 20)
+  }
+  return(output)
 }
+
+paired_output <- neonatal_SAM(dfs, y, resp.type = "Two class paired", SAMDirectory <- "~/Documents/neonatal/SAM/")
+# unpaired_output <- neonatal_SAM(dfs, y = ifelse(sign(y) > 0, 2, 1), resp.type = "Two class unpaired", SAMDirectory <- "~/Documents/neonatal/SAM/SAM_paired_v_unpaired/")
+
 
 ## Abundances! % mononuclear
 # generated percents_df (includes annotations) and percents (numeric only) from neonatals data structures
--- R/neonatals-data-structures.R
+++ R/neonatals-data-structures.R
@@ -3,11 +3,13 @@
 # Processing neonatal data (QBaca)
 
 library(dplyr)
+library(blackbuck)
 library(FDAlibrary)
 library(ggplot2)
 library(samr)
 
-# read in signaling data
+#-------------Signaling Data Structures----------------#
+
 dataDirectory <- "/Users/gabrielafragiadakis/Documents/neonatal/exported_statistics/"
 raw_df <- read.csv(paste(dataDirectory, "neonatals-medians.csv", sep = ""))
 
@@ -29,12 +31,13 @@ unstim_df <- reshape2::melt(unstim_df)
 unstim_df$value <- asinh(unstim_df$value / 5)
 unstim_df <- reshape2::dcast(unstim_df, ... ~ variable)
 unstim_df <- dplyr::select(unstim_df, -filename)
+Unstim_df <- unstim_df
 
 
+# building blocks: unstim_df, LPS_df, Cocktail_df
+# Add condition names to features
 annotation_columns = c("Individual", "Pair","SampleType", "Condition")
 
-
-# Add conditions onto it
 LPS_df <- dplyr::filter(fold_df, Condition == "LPS")
 colnames(LPS_df) <- paste(colnames(LPS_df), "_LPS", sep = "")
 colnames(LPS_df)[1:4] <- annotation_columns
@@ -42,11 +45,10 @@ Cocktail_df <- dplyr::filter(fold_df, Condition == "Cocktail")
 colnames(Cocktail_df) <- paste(colnames(Cocktail_df), "_Cocktail", sep = "")
 colnames(Cocktail_df)[1:4] <- annotation_columns
 new_df <- cbind(LPS_df, Cocktail_df[,!(names(Cocktail_df) %in% annotation_columns)])
+  
 
-# now have new_df for fold changed stims, and unstim_df for unstim.  Should run SAM two-class paired on both.  
-# can also do unstim_df, LPS_df, Cocktail_df
 
-# abundance data
+##-----------abundance data----------## 
 
 abundance_df <- read.csv(paste(dataDirectory, "neonatals-abundances.csv", sep = ""))
 
@@ -59,11 +61,12 @@ abundance_df <- abundance_df %>%
 
 base_counts <- abundance_df$Mononuclearcells_EventCounts
 singlet_counts <- abundance_df$Singlets_EventCounts
-cell_type_cols <- colnames(abundance_df)[7:23]
-cell_types <- abundance_df[, colnames(abundance_df) %in% cell_type_cols]
+cell_types <- abundance_df[,!(names(abundance_df) %in% c(annotation_columns, 
+                                                         "Singlets_EventCounts", "Mononuclearcells_EventCounts", 
+                                                         "Granulocytes_EventCounts", "filename"))]
 grans <- abundance_df$Granulocytes_EventCounts
 percents <- cbind(cell_types/base_counts, grans/singlet_counts)
-percents_df <- data.frame(abundance_df[,c(1:4, 25)], percents)
-write.csv(percents_df, file = "~/Documents/neonatal/neonatals_percentages.csv", rownames = FALSE)
+percents_df <- data.frame(filename = abundance_df$filename, abundance_df[, colnames(abundance_df) %in% annotation_columns], percents)
+# write.csv(percents_df, file = "~/Documents/neonatal/neonatals_percentages.csv")
 
 
